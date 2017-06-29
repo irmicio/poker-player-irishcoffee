33b73c37e55341c0d91736be8a8319205c5d833d
ACardoso <antonio.cardoso@adcubum.com>
2017-06-29 11:50:06 +0200

    bet

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,7 +1,10 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
+import java.math.BigInteger;
 import java.util.Map;
 
 public class Player {
@@ -9,7 +12,24 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+//    	JsonArray asJsonArray = request.getAsJsonArray();
+//    	JsonElement jsonElement = asJsonArray.get(0);
+    	JsonObject asJsonObject = request.getAsJsonObject();
+    	JsonElement playersElement = asJsonObject.get("players");
+    	JsonArray asJsonArray = playersElement.getAsJsonArray();
+    	
+    	int myBet  = 0;
+    	
+    	for (int i = 0; i < asJsonArray.size(); i++) {
+    		JsonObject jsonElement = asJsonArray.get(i).getAsJsonObject();
+    		BigInteger bet = jsonElement.get("bet").getAsBigInteger();
+    		myBet = Math.max(myBet, bet.intValue());
+		}
+    	myBet++;
+//    	JsonElement jsonElement = playersElement.getAsJsonObject().get("bet");
+//    	System.out.println(playersElement);
+//    	jsonElement.get
+        return myBet;
     }
 
     public static void showdown(JsonElement game) {
