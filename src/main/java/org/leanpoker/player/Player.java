ba63e91a9f6de7c56d4239eb3b9fe2c506372d03
ACardoso <antonio.cardoso@adcubum.com>
2017-06-29 13:50:32 +0200

    nicht null

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -17,10 +17,14 @@ public class Player {
 
     	JsonObject asJsonObject = request.getAsJsonObject();
     	JsonElement playersElement = asJsonObject.get("players");
+    	int round = asJsonObject.get("round").getAsBigInteger().intValue();
+    	int current_buy_in = asJsonObject.get("current_buy_in").getAsBigInteger().intValue();
     	JsonArray asJsonArray = playersElement.getAsJsonArray();
+
+    	JsonArray comunityCards = asJsonObject.get("community_cards").getAsJsonArray();
     	
     	int myBet  = 0;
-    	
+    	int myBetStep = 10;
     	int mystack = 1000;
     	
     	for (int i = 0; i < asJsonArray.size(); i++) {
@@ -35,14 +39,34 @@ public class Player {
         		if (card1.get("rank").equals(card2.get("rank"))) {
         			return Math.max(0, mystack);
         		}
+        		if (card1.get("suit").equals(card2.get("suit"))) {
+        			int found = 0;
+        			for (int j = 0; j < comunityCards.size(); j++) {
+        				if (card1.get("suit").equals(comunityCards.get(j).getAsJsonObject().get("suit").getAsString())){
+        					found++;
+        				}
+					}
+        			if (found == 3) {
+            			return Math.max(0, mystack);
+        			}
+        			if (found == 2 && round == 3) {
+            			return 0;
+        			}
+        			if (found == 2 && round == 1) {
+        				myBetStep = 100;
+        			}
+        			else {
+        				myBetStep = 50;
+        			}
+        		}
     			continue;
     		}
     		myBet = Math.max(myBet, bet.intValue());
 		}
-    	myBet++;
+    	myBet = myBet + myBetStep;
     	if (myBet >= mystack)
     	{
-    		return 0;
+    		return Math.min(current_buy_in, mystack);
     	}
         return Math.max(0, myBet);
     }
